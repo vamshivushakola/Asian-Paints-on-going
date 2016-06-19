ecca70da7e29bd879d371fce00fe856118f9987e
vamshivushakola <vamshi.vshk@gmail.com>
2016-06-19 15:20:04 +0530

    B2C{B2C sotre,OMS,PayPal,Coming Soon,Addons,Registration,Promotions, Vouchers}

---
+++ asianpaints/asianpaintsfacades/gensrc/com/asianpaints/facades/constants/GeneratedAsianpaintsFacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jun 18, 2016 6:07:30 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.asianpaints.facades.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedAsianpaintsFacadesConstants
+{
+	public static final String EXTENSIONNAME = "asianpaintsfacades";
+	
+	protected GeneratedAsianpaintsFacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ paypalcscockpit/gensrc/com/paypal/hybris/constants/GeneratedPaypalcscockpitConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jun 18, 2016 6:07:30 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.paypal.hybris.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedPaypalcscockpitConstants
+{
+	public static final String EXTENSIONNAME = "paypalcscockpit";
+	
+	protected GeneratedPaypalcscockpitConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
